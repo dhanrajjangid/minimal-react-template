f187b7215f1eae90971006007fba3917b6be41c5
dhanraj jangid <dhanrajjangidst@gmail.com>
2023-12-18 20:58:31 +0530

    product responsiveness issue resolve

--- src/view/Products/index.jsx
+++ src/view/Products/index.jsx
@@ -1,62 +1,62 @@
-import React from 'react';
-import styled from 'styled-components';
-import ProductCard from '@/view/Products/components/ProductCard';
+import React from "react";
+import styled from "styled-components";
+import ProductCard from "@/view/Products/components/ProductCard";
 
 const items = [
   {
     id: 1,
-    title: 'Running Shoes',
+    title: "Running Shoes",
     price: 69.99,
-    image: 'https://images.pexels.com/photos/936019/pexels-photo-936019.jpeg',
+    image: "https://images.pexels.com/photos/936019/pexels-photo-936019.jpeg",
     ratings: 4.5,
   },
   {
     id: 2,
-    title: 'Sports T-Shirt',
+    title: "Sports T-Shirt",
     price: 29.99,
-    image: 'https://images.pexels.com/photos/933076/pexels-photo-933076.jpeg',
+    image: "https://images.pexels.com/photos/933076/pexels-photo-933076.jpeg",
     ratings: 4,
   },
   {
     id: 3,
-    title: 'Sportswear Set',
+    title: "Sportswear Set",
     price: 79.99,
-    image: 'https://images.pexels.com/photos/1170975/pexels-photo-1170975.jpeg',
+    image: "https://images.pexels.com/photos/1170975/pexels-photo-1170975.jpeg",
     ratings: 3.5,
   },
   {
     id: 4,
-    title: 'Athletic Shorts',
+    title: "Athletic Shorts",
     price: 39.99,
-    image: 'https://images.pexels.com/photos/965343/pexels-photo-965343.jpeg',
+    image: "https://images.pexels.com/photos/965343/pexels-photo-965343.jpeg",
     ratings: 3.5,
   },
   {
     id: 5,
-    title: 'Running Jacket',
+    title: "Running Jacket",
     price: 49.99,
-    image: 'https://images.pexels.com/photos/1428663/pexels-photo-1428663.jpeg',
+    image: "https://images.pexels.com/photos/1428663/pexels-photo-1428663.jpeg",
     ratings: 3.5,
   },
   {
     id: 6,
-    title: 'Yoga Leggings',
+    title: "Yoga Leggings",
     price: 44.99,
-    image: 'https://images.pexels.com/photos/2872904/pexels-photo-2872904.jpeg',
+    image: "https://images.pexels.com/photos/2872904/pexels-photo-2872904.jpeg",
     ratings: 3.5,
   },
   {
     id: 7,
-    title: 'Compression Socks',
+    title: "Compression Socks",
     price: 19.99,
-    image: 'https://images.pexels.com/photos/3394060/pexels-photo-3394060.jpeg',
+    image: "https://images.pexels.com/photos/3394060/pexels-photo-3394060.jpeg",
     ratings: 3.5,
   },
   {
     id: 8,
-    title: 'Sweatband Set',
+    title: "Sweatband Set",
     price: 15.99,
-    image: 'https://images.pexels.com/photos/3486892/pexels-photo-3486892.jpeg',
+    image: "https://images.pexels.com/photos/3486892/pexels-photo-3486892.jpeg",
     ratings: 3.5,
   },
 ];
@@ -66,7 +66,7 @@ const Container = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   gap: 16px;
-  padding: 16px;
+  padding: 4px;
 `;
 
 const Products = () => {
@@ -79,4 +79,4 @@ const Products = () => {
   );
 };
 
-export default Products
+export default Products;
