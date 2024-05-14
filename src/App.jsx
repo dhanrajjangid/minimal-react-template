d65b6583a21144dd46b96d33c40508efd12d191f
dhanrajjangid22 <dhanraj.jangid@ignatiuz.com>
2024-05-14 12:47:13 +0530

    meta pixel testing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "@/routes";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
+import ReactPixel from "react-facebook-pixel";
 
 function App() {
+  useEffect(() => {
+    ReactPixel.init("TEST84903");
+  }, []);
+
   return (
     <div>
       <Provider store={store}>
--- src/view/Products/index.jsx
+++ src/view/Products/index.jsx
@@ -1,62 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import ProductCard from "@/view/Products/components/ProductCard";
+import ReactPixel from 'react-facebook-pixel';
 
 const items = [
   {
     id: 1,
     title: "Running Shoes",
     price: 69.99,
-    image: 'https://source.unsplash.com/300x300/?product',
+    image: "https://source.unsplash.com/300x300/?product",
     ratings: 4.5,
   },
   {
     id: 2,
     title: "Sports T-Shirt",
     price: 29.99,
-    image: 'https://source.unsplash.com/300x300/?product',
+    image: "https://source.unsplash.com/300x300/?product",
     ratings: 4,
   },
   {
     id: 3,
     title: "Sportswear Set",
     price: 79.99,
-    image: 'https://source.unsplash.com/300x300/?product',
+    image: "https://source.unsplash.com/300x300/?product",
     ratings: 3.5,
   },
   {
     id: 4,
     title: "Athletic Shorts",
     price: 39.99,
-    image: 'https://source.unsplash.com/300x300/?product',
+    image: "https://source.unsplash.com/300x300/?product",
     ratings: 3.5,
   },
   {
     id: 5,
     title: "Running Jacket",
     price: 49.99,
-    image: 'https://source.unsplash.com/300x300/?product',
+    image: "https://source.unsplash.com/300x300/?product",
     ratings: 3.5,
   },
   {
     id: 6,
     title: "Yoga Leggings",
     price: 44.99,
-    image: 'https://source.unsplash.com/300x300/?product',
+    image: "https://source.unsplash.com/300x300/?product",
     ratings: 3.5,
   },
   {
     id: 7,
     title: "Compression Socks",
     price: 19.99,
-    image: 'https://source.unsplash.com/300x300/?product',
+    image: "https://source.unsplash.com/300x300/?product",
     ratings: 3.5,
   },
   {
     id: 8,
     title: "Sweatband Set",
     price: 15.99,
-    image: 'https://source.unsplash.com/300x300/?product',
+    image: "https://source.unsplash.com/300x300/?product",
     ratings: 3.5,
   },
 ];
@@ -72,6 +73,10 @@ const Container = styled.div`
 `;
 
 const Products = () => {
+  useEffect(() => {
+    ReactPixel.track("Purchase", { currency: "USD", value: 30.0 });
+  }, []);
+
   return (
     <Container>
       {items.map((product) => (
